cbda3b9d16f7299135eef930bfba67e1d611c8ea
tim tucker <timothytucker@berkeley.edu>
2021-02-19 11:31:44 -0800

    testing csv file for automated loading according to timing sheet

--- script.py
+++ script.py
@@ -1,13 +1,27 @@
 import webbrowser
 import os
 import pandas as pd
+from datetime import datetime
 
-def open_link():
-    zoom_link = 'https://berkeley.zoom.us/j/99744847516?pwd=VEFDYS9kR1JoWG1JWVUrOEVZamVWQT09'
+def open_link(zoom_link):
     chrome_path = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+
     webbrowser.register('google_chrome', None, webbrowser.BackgroundBrowser(chrome_path))
     os.system(r'C:\Program Files\Google\Chrome\Application\chrome.exe')
-    webbrowser.get('google_chrome').open('https://berkeley.zoom.us/j/99744847516?pwd=VEFDYS9kR1JoWG1JWVUrOEVZamVWQT09')
-    #webbrowser.get(chrome_path).open('https://ocf.io/~timtucker')
 
-open_link()
+    webbrowser.get('google_chrome').open(zoom_link)
+
+
+timingSheet = pd.read_csv('zooms.csv')
+while True:
+    now = datetime.now()
+    nowTime = now.strftime("%H:%M")
+    dayOfWeek = now.weekday()
+    if nowTime in str(timingSheet['times']):
+        row = timingSheet.loc[timingSheet['times'] == now]
+        zoomDay = int(row.iloc[:,0])
+        if zoomDay == dayOfWeek:
+            zoomLink = row.iloc[:,2]
+            open_link(zoomLink)
+            time.sleep(60)
+            print('joined zoom')
